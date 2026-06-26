e57917c199d5982237e99b335bdaf9b90cf65047
Peter Butzelaar <peter1.butzelaar@vattenfall.com>
2026-06-26 15:08:07 +0200

    Add dynamic due date cards

---
+++ src/flow/action/addPlannedPersonTaskDynamic.ts
@@ -0,0 +1,29 @@
+import { action, DateTime, FlowActionEntity } from '@basmilius/homey-common';
+import type { BasicListDevice } from '../../list';
+import type { ListriApp } from '../../types';
+import { AutocompleteProviders } from '..';
+
+@action('add_planned_person_task_dynamic')
+export default class extends FlowActionEntity<ListriApp, Args> {
+    async onInit(): Promise<void> {
+        this.registerAutocomplete('person', AutocompleteProviders.Person);
+
+        await super.onInit();
+    }
+
+    async onRun(args: Args): Promise<void> {
+        await args.list.addTask(args.task, args.date, args.time, args.person);
+    }
+}
+
+type Args = {
+    readonly list: BasicListDevice;
+    readonly task: string;
+    readonly date: string;
+    readonly time?: string;
+    readonly person: {
+        readonly id: string;
+        readonly name: string;
+        readonly image: string | undefined;
+    };
+};
---
+++ src/flow/action/addPlannedTaskDynamic.ts
@@ -0,0 +1,17 @@
+import { action, DateTime, FlowActionEntity } from '@basmilius/homey-common';
+import type { BasicListDevice } from '../../list';
+import type { ListriApp } from '../../types';
+
+@action('add_planned_task_dynamic')
+export default class extends FlowActionEntity<ListriApp, Args> {
+    async onRun(args: Args): Promise<void> {
+        await args.list.addTask(args.task, args.date, args.time);
+    }
+}
+
+type Args = {
+    readonly list: BasicListDevice;
+    readonly task: string;
+    readonly date: string;
+    readonly time?: string;
+};
