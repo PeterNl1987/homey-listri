fe05155659713ab2bd7f70b6c6d2f223e6e9f74e
Peter Butzelaar <peter1.butzelaar@vattenfall.com>
2026-06-30 13:54:56 +0200

    Homey SDK runs in UTC. Convert today to local time

--- src/flow/action/addPlannedTaskInDaysNoTime.ts
+++ src/flow/action/addPlannedTaskInDaysNoTime.ts
@@ -5,12 +5,26 @@ import type { ListriApp } from '../../types';
 @action('add_planned_task_in_days_no_time')
 export default class extends FlowActionEntity<ListriApp, Args> {
     async onRun(args: Args): Promise<void> {
+        // Homey SDK runs in UTC time, not in local time. So to get today as local time, we have to get the Homey timezone and do below logic.
         var date = new Date();
         date.setDate(date.getDate() + args.days);
-        var dateString = 
-            date.getFullYear()
-            + "-" + String(date.getMonth() + 1).padStart(2, '0')
-            + "-" + String(date.getDate()).padStart(2, '0')
+        const timezone = this.homey.clock.getTimezone();
+
+        const parts = new Intl.DateTimeFormat(undefined, {
+            timeZone: timezone,
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric'
+        }).formatToParts(date);
+
+        const day = Number(parts.find(p => p.type === 'day')?.value);
+        const month = Number(parts.find(p => p.type === 'month')?.value);
+        const year = Number(parts.find(p => p.type === 'year')?.value);
+
+        var dateString =
+            year
+            + "-" + String(month + 1).padStart(2, '0')
+            + "-" + String(day).padStart(2, '0')
 
         await args.list.addTask(args.task, dateString);
     }
