ba361760abcead7b0005587090054e79862e5a92
Peter Butzelaar <peter1.butzelaar@vattenfall.com>
2026-06-30 14:23:03 +0200

    Easy on due date

--- src/flow/action/addPlannedPersonTaskInDays.ts
+++ src/flow/action/addPlannedPersonTaskInDays.ts
@@ -12,28 +12,10 @@ export default class extends FlowActionEntity<ListriApp, Args> {
     }
 
     async onRun(args: Args): Promise<void> {
-        // Homey SDK runs in UTC time, not in local time. So to get today as local time, we have to get the Homey timezone and do below logic.
-        var date = new Date();
-        date.setDate(date.getDate() + args.days);
-        const timezone = this.homey.clock.getTimezone();
-
-        const parts = new Intl.DateTimeFormat(undefined, {
-            timeZone: timezone,
-            year: 'numeric',
-            month: 'numeric',
-            day: 'numeric'
-        }).formatToParts(date);
-
-        const day = Number(parts.find(p => p.type === 'day')?.value);
-        const month = Number(parts.find(p => p.type === 'month')?.value);
-        const year = Number(parts.find(p => p.type === 'year')?.value);
-
-        var dateString =
-            year
-            + "-" + String(month + 1).padStart(2, '0')
-            + "-" + String(day).padStart(2, '0')
-
-        await args.list.addTask(args.task, dateString, args.time, args.person);
+        var dateTime = DateTime.now();
+        dateTime = dateTime.plus({ days: args.days });
+        
+        await args.list.addTask(args.task, dateTime.toFormat("yyyy-MM-dd"), args.time, args.person);
     }
 }
 
--- src/flow/action/addPlannedTaskInDays.ts
+++ src/flow/action/addPlannedTaskInDays.ts
@@ -1,32 +1,14 @@
-import { action, FlowActionEntity } from '@basmilius/homey-common';
+import { action, DateTime, FlowActionEntity } from '@basmilius/homey-common';
 import type { BasicListDevice } from '../../list';
 import type { ListriApp } from '../../types';
 
 @action('add_planned_task_in_days')
 export default class extends FlowActionEntity<ListriApp, Args> {
     async onRun(args: Args): Promise<void> {
-        // Homey SDK runs in UTC time, not in local time. So to get today as local time, we have to get the Homey timezone and do below logic.
-        var date = new Date();
-        date.setDate(date.getDate() + args.days);
-        const timezone = this.homey.clock.getTimezone();
-
-        const parts = new Intl.DateTimeFormat(undefined, {
-            timeZone: timezone,
-            year: 'numeric',
-            month: 'numeric',
-            day: 'numeric'
-        }).formatToParts(date);
-
-        const day = Number(parts.find(p => p.type === 'day')?.value);
-        const month = Number(parts.find(p => p.type === 'month')?.value);
-        const year = Number(parts.find(p => p.type === 'year')?.value);
-
-        var dateString =
-            year
-            + "-" + String(month + 1).padStart(2, '0')
-            + "-" + String(day).padStart(2, '0')
-
-        await args.list.addTask(args.task, dateString, args.time);
+                var dateTime = DateTime.now();
+                dateTime = dateTime.plus({ days: args.days });
+                
+                await args.list.addTask(args.task, dateTime.toFormat("yyyy-MM-dd"), args.time);
     }
 }
 
